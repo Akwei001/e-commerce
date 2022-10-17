0734e8996a383f0fdf08dfaa351b12caf5066eba
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-10-17 18:19:43 +0100

    product fetch display

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ const Home = ({ products, bannerData }) => {
         <p>A great Range of Speakers</p>
       </div>
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
