c569d6c25e4128c2fac94cd71842fa63c268fce7
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-10-24 11:02:55 +0100

    product page image issue

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,14 +1,48 @@
 import React from 'react';
-
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from 'react-icons/ai';
 import { client, urlFor } from '../../lib/client';
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+  const { image, name, details, price } = product;
   return (
     <div>
       <div className='product-detail-container'>
         <div>
           <div className='image-container'>
-            <img src='' />
+            <img
+              src={urlFor(image && image[0])}
+              className='product-detail-image'
+            />
+          </div>
+          {/* <div className='small-images-container'>
+            {image?.map((item, i) => (
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={
+                  i === index ? 'small-image selected-image' : 'small-image'
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div> */}
+        </div>
+        <div className='product-details-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
           </div>
         </div>
       </div>
