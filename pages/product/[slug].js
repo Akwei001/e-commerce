72bacd375fdfb1ebe9a844f0667623a276293391
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-10-28 21:37:46 +0100

    product page css fixed

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,14 +5,22 @@ import {
   AiFillStar,
   AiOutlineStar,
 } from 'react-icons/ai';
+
+import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
 import { useStateContext } from '../../context/StateContext';
-import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+  // const handleBuyNow = () => {
+  //   onAdd(product, qty);
+
+  //   setShowCart(true);
+  // }
+
   return (
     <div>
       <div className='product-detail-container'>
@@ -36,7 +44,8 @@ const ProductDetails = ({ product, products }) => {
             ))}
           </div>
         </div>
-        <div className='product-details-desc'>
+
+        <div className='product-detail-desc'>
           <h1>{name}</h1>
           <div className='reviews'>
             <div>
@@ -50,7 +59,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <h4>Details: </h4>
           <p>{details}</p>
-          <p className='price'>£{price}</p>
+          <p className='price'>${price}</p>
           <div className='quantity'>
             <h3>Quantity:</h3>
             <p className='quantity-desc'>
@@ -79,7 +88,7 @@ const ProductDetails = ({ product, products }) => {
       </div>
 
       <div className='maylike-products-wrapper'>
-        <h2> You may also like</h2>
+        <h2>You may also like</h2>
         <div className='marquee'>
           <div className='maylike-products-container track'>
             {products.map((item) => (
